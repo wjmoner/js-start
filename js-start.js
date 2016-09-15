7206b3fd6bd65ad12e164411ac51f9f121bee34b
William Moner <wmoner@elon.edu>
2016-09-15 12:01:44 -0400

    Initial repo for js-start
    Initial repo for js-start

---
+++ js-start.js
@@ -0,0 +1,15 @@
+// javascript goes here
+
+var init = function () { // you may also see this written as function init() { ...
+
+	// your js code goes here
+
+
+
+
+
+
+	// your js code ends here
+}; // end of init function
+
+init(); // function call to "init"
